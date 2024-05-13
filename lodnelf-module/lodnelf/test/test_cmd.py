65aecb1aff13e90b65687e8b0ba8a9d922e72e6a
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-05-13 15:07:06 +0200

    Adding alpha and depth plotting options

--- lodnelf-module/lodnelf/test/test_cmd.py
+++ lodnelf-module/lodnelf/test/test_cmd.py
@@ -7,7 +7,7 @@ from lodnelf.train.config.config_factory import ConfigFactory
 import argparse
 
 
-def run_testing(config_name, model_path, data_dir):
+def run_testing(config_name, model_path, data_dir, plot_alpha=False, plot_depth=False):
     config_factory = ConfigFactory()
     config = config_factory.get_by_name(config_name)
 
@@ -20,7 +20,25 @@ def run_testing(config_name, model_path, data_dir):
 
     dataset = config.get_data_set(data_dir)
 
-    fig, axs = plt.subplots(2, 10, figsize=(18, 3))
+    n_rows = 2
+
+    if plot_alpha:
+        n_rows = n_rows + 1
+
+    if plot_depth:
+        n_rows = n_rows + 1
+
+    fig, axs = plt.subplots(n_rows, 10, figsize=(18, n_rows * 1.7))
+    fig.tight_layout()
+    axs[0][0].set_ylabel("Actual")
+    axs[1][0].set_ylabel("Predicted RGB")
+
+    if plot_alpha:
+        axs[2][0].set_ylabel("Predicted Alpha")
+
+    if plot_depth:
+        axs[n_rows - 1][0].set_ylabel("Predicted Depth")
+
     for i in range(10):
         query = dataset[i]
         model_input = util.add_batch_dim_to_dict(
@@ -28,8 +46,21 @@ def run_testing(config_name, model_path, data_dir):
         )
         output = model(model_input)
 
-        axs[0][i].imshow(output["rgb"].reshape(128, 128, 3).detach().cpu().numpy())
-        axs[1][i].imshow(query["rgb"].reshape(128, 128, 3).detach().cpu().numpy())
+        axs[0][i].imshow(query["rgb"].reshape(128, 128, 3).detach().cpu().numpy())
+        axs[1][i].imshow(output["rgb"].reshape(128, 128, 3).detach().cpu().numpy())
+
+        if "alpha" in output and plot_alpha:
+            axs[2][i].imshow(
+                output["alpha"].reshape(128, 128, 1).detach().cpu().numpy(),
+                cmap="gray",
+            )
+
+        if "depth" in output and plot_depth:
+            axs[n_rows - 1][i].imshow(
+                output["depth"].reshape(128, 128, 1).detach().cpu().numpy(),
+                cmap="gray",
+            )
+
     plt.show()
 
 
@@ -57,6 +88,28 @@ if __name__ == "__main__":
         help="Path to the data directory.",
     )
 
+    args.add_argument(
+        "--plot_alpha",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Whether or not to plot alpha channel",
+    )
+
+    args.add_argument(
+        "--plot_depth",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Whether or not to plot depth",
+    )
+
     args = args.parse_args()
 
-    run_testing(args.config, args.model_save_path, args.data_dir)
+    run_testing(
+        args.config,
+        args.model_save_path,
+        args.data_dir,
+        args.plot_alpha,
+        args.plot_depth,
+    )
