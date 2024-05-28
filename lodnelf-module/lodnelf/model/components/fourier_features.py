2a9bbc8d1101c434439e4e135d3d6faf11acae08
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-05-28 17:44:39 +0200

    Do not require grad on fourier features

--- lodnelf-module/lodnelf/model/components/fourier_features.py
+++ lodnelf-module/lodnelf/model/components/fourier_features.py
@@ -7,9 +7,11 @@ class FourierFeatures(nn.Module):
     def __init__(self, mapping_size: int):
         super(FourierFeatures, self).__init__()
         self.exp = 2 ** torch.arange(0, mapping_size).float() * np.pi
-        self.exp = nn.Parameter(self.exp)
+        self.exp = nn.Parameter(self.exp, requires_grad=False)
 
     def forward(self, x):
         b, q, d = x.shape
-        x_proj = torch.einsum("bqd, p -> bqdp", x, self.exp).view(b, q, d * len(self.exp))
+        x_proj = torch.einsum("bqd, p -> bqdp", x, self.exp).view(
+            b, q, d * len(self.exp)
+        )
         return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
