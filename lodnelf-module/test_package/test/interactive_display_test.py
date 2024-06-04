321c0ad1becc817e72063a2e9866d854ff7715e3
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-06-04 13:51:57 +0200

    Try to add subsampled model

--- lodnelf-module/test_package/test/interactive_display_test.py
+++ lodnelf-module/test_package/test/interactive_display_test.py
@@ -44,3 +44,14 @@ class InteractiveDisplayTest(unittest.TestCase):
         )
 
         interactive_display.run()
+
+    def test_given_a_valid_subsampled_plucker_config_with_train_val_split_and_model_path__when_running__then_run_the_application(
+        self,
+    ):
+        interactive_display = InteractiveDisplay(
+            "DeepNeuralNetworkPluckerLegoLarge",
+            "models/lego_deep_plucker_3_subsampled_2/model_epoch_33.pt",
+            mode="rgba",
+        )
+
+        interactive_display.run()
